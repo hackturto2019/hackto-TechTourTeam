3ca9fcb63c013766f2fccd95b34c1bc707a13cf6
fergomess <fernandagomes769@gmail.com>
2019-10-04 18:57:49 -0300

    Logo e Dados Rotas

--- src/app/rotas/rotas.component.ts
+++ src/app/rotas/rotas.component.ts
@@ -8,15 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class RotasComponent implements OnInit {
 
   rotas = Array(
-      {'titulo': 'Rota das Cachoeiras',
-        'texto':'Conheça as cachoeiras e vistas da região',
-        'imagem-destaque':'../../assets/img/cachoeira.jpg'},
-      {'titulo':'Restaurantes Vegetariano/Vegano',
-        'texto':'Os melhores restaurantes com opções vegetarianas', 'imagem-destaque':'../../assets/img/cachoeira.jpg'},
-      {'titulo':'Praias e Natureza',
-        'texto':'Lazer e descanço',
-        'imagem-destaque': '../../assets/img/cachoeira.jpg'}
-      );
+      {
+          'titulo': 'Rota das Cachoeiras',
+          'texto':'Conheça as cachoeiras e vistas da região',
+          'imagem-destaque':'../../assets/img/cachoeira.jpg',
+          'valor':'80',
+          'distancia': '20',
+          'tempo': '2 horas',
+          'pontos': '100'
+      },
+      {
+          'titulo':'Restaurantes Vegetariano/Vegano',
+          'texto':'Os melhores restaurantes com opções vegetarianas',
+          'imagem-destaque':'../../assets/img/restaurante.jpg',
+          'valor':'160',
+          'distancia': '90',
+          'tempo': '3 Dias',
+          'pontos': '80'
+      },
+      {
+          'titulo':'Praias e Natureza',
+          'texto':'Lazer e descanço',
+          'imagem-destaque': '../../assets/img/cachoeira.jpg',
+          'valor':'300',
+          'distancia': '20',
+          'tempo': '5h',
+          'pontos': '150'
+      });
   constructor() {}
 
   ngOnInit() {
